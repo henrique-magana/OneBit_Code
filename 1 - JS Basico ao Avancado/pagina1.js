01fa28ee2759c71c83292f48e6199c1ea9c58990
Henrique Magana <henrique.magana@bdo.com.br>
2024-01-15 17:21:41 -0300

    comecando

---
+++ 1 - JS Basico ao Avancado/pagina1.js
@@ -0,0 +1,12 @@
+let nome = 'henrique'
+
+console.log(nome)
+
+console.error(nome)
+
+console.warn(nome)
+
+
+nome = window.prompt("Coloque um valor")
+
+window.alert(nome)
---
+++ 1 - JS Basico ao Avancado/pagina2.js
@@ -0,0 +1,15 @@
+
+
+let n1, n2, soma, sub, mult, div
+
+n1 = window.prompt("Valor 1")
+n1 = window.prompt("Valor 2")
+soma = n1 + n2
+
+sub = n1 - n2
+
+mult = n1* n2
+
+div = n1 / n2
+
+window.alert("Soma "+ soma + " Subtração "+ sub+ " Multiplicação " + mult + " Div " + div)
